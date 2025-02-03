22fb875bbb3505792cd5b7cd3d90a5ccbb9ddd64
hossein <arkittoe@gmail.com>
2025-02-03 14:41:28 +0330

    add all

---
+++ DOR/DOR.bat
@@ -0,0 +1,4 @@
+
+echo off
+CALL "C:\Program Files (x86)\AVEVA\Everything3D3.1\mon.exe" PROD E3D init "C:\Program Files (x86)\AVEVA\Everything3D3.1\launch.init" GRAPHICS DOR SYSTEM/XXXXXX /DOROOD-ALL-PLANT $M/Y:\ADMIN\Z-IZADI\EXPORT_WITH_FULL_ATT\NAVIS\DOR\DOR.mac
+
---
+++ P12/P12-NWD.bat
@@ -0,0 +1,4 @@
+
+echo off
+CALL "C:\Program Files (x86)\AVEVA\Everything3D3.1\mon.exe" PROD E3D init "C:\Program Files (x86)\AVEVA\Everything3D3.1\launch.init" GRAPHICS PFI PASHAEI/PA12 /NEW-NEW-P12 $M/Y:\ADMIN\Z-IZADI\EXPORT_WITH_FULL_ATT\FINAL\EXPORT-ATT-tuatt-RVM\P12\P12.mac
+
